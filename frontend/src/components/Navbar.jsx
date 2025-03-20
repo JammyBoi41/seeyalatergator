533a72312613557eb98419fe630a5c318ba049b3
Jamin Chung <jamin.chung@ufl.edu>
2025-03-19 21:46:03 -0400

    initial landing page prototype in the works

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-black w-screen h-10 fixed top-0 left-0'>
+    <div className='bg-white w-screen h-15 fixed top-0 left-0 border-gray-200 border-b-1'>
         
     </div>
   )
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div style={{backgroundColor: "#f2f4fa"}} className="w-full h-screen flex flex-row items-center">  {/* landing page div */}
+        <div className="flex flex-col gap-y-2 pl-15 pr-15"> {/* left half */}
+            <h1 className="font-bold text-7xl text-orange-500"> See Ya <span className="text-blue-800">Later Gator</span></h1>
+            <h2 className="text-5xl text-gray-600"> By Gators, For Gators</h2>
+        </div>
+
+
+        <div>
+          
+        </div>
+    </div>
   )
 }
 
