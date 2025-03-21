1ae483df4116b8051bd0e0a10c6c3252746e2899
Jamin Chung <jamin.chung@ufl.edu>
2025-03-20 20:34:52 -0400

    started working on login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div style={{backgroundColor: "#f2f4fa"}} className="w-full h-screen flex flex-col items-center justify-center">  {/* landing page div */}
+      
+      <div> {/* top label */}
+        <h1 className="text-3xl font-bold text-gray-800 mb-2"> Welcome Back </h1>
+      </div>
+
+      <Form className="w-1/4 bg-white rounded border-gray-200 border-1 pl-7 pr-7 pt-5 pb-5 font-bold">
+        <Form.Group className="flex flex-col mb-5">
+          <Form.Label className="text-sm">
+            Email Address
+          </Form.Label>
+          <Form.Control placeholder="name@example.com" className="outline-gray-400 border-gray-300 border-1 rounded h-9 font-thin pl-3 text-sm"/>
+        </Form.Group>
+
+
+        <Form.Group className="flex flex-col mb-5">
+          <Form.Label className="text-sm">
+            Email Address
+          </Form.Label>
+          <Form.Control placeholder="name@example.com" type="password" className="outline-gray-400 border-gray-300 border-1 rounded h-9 font-thin pl-3 text-sm"/>
+        </Form.Group>
+
+        <button className="bg-gray-950 w-full text-white pt-2 pb-2 rounded-xl hover:bg-gray-700 transition duration-300 cursor-pointer">
+          Log In
+        </button>
+    </Form>
+    </div>
   )
 }
 
